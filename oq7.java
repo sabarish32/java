b5de80dc3919712dc6ab21bd3322d6ef701bca49
sabarish32 <sabarish23304@gmail.com>
2025-12-13 11:19:54 +0530

    Day3

--- oq7.java
+++ oq7.java
@@ -1,3 +1,19 @@
-public class oq7 {
-    
+import java.util.Scanner;
+public class oq7{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+
+        System.out.println(a);
+         System.out.println(b);
+
+
+
+        
+    }
 }
--- oq9.java
+++ oq9.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 public class oq9 {
-    
-    
+    public static void main(String[]args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+
+        int result= n/10;
+        System.out.println(result);
+    }
 }
