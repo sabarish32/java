c8009473915bb5d885aed2f3d1b3e5b4a26dd743
sabarish32 <sabarish23304@gmail.com>
2025-12-13 15:43:47 +0530

    day 3

---
+++ conditional statement/SQ1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class SQ1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+': System.out.println(a + b); break;
+            case '-': System.out.println(a - b); break;
+            case '*': System.out.println(a * b); break;
+            case '/': System.out.println(a / b); break;
+            case '%': System.out.println(a % b); break;
+            default: System.out.println("Invalid Operator");
+        }
+    }
+}
---
+++ conditional statement/SQ3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class SQ3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+
+        switch (marks / 10) {
+            case 10:
+            case 9:
+                System.out.println("Grade A");
+                break;
+            case 8:
+                System.out.println("Grade B");
+                break;
+            case 7:
+                System.out.println("Grade C");
+                break;
+            default:
+                System.out.println("Fail");
+        }
+    }
+}
