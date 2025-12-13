86e03f0ed11a4d4261ec313b34ff77c1246563dd
sabarish32 <sabarish23304@gmail.com>
2025-12-13 15:21:57 +0530

    day 3

---
+++ conditional statement/GQ3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class GQ3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if (x > 0 && y > 0)
+            System.out.println("First Quadrant");
+        else if (x < 0 && y > 0)
+            System.out.println("Second Quadrant");
+        else if (x < 0 && y < 0)
+            System.out.println("Third Quadrant");
+        else if (x > 0 && y < 0)
+            System.out.println("Fourth Quadrant");
+        else if (x == 0 && y == 0)
+            System.out.println("Origin");
+        else if (x == 0)
+            System.out.println("On Y-axis");
+        else
+            System.out.println("On X-axis");
+    }
+}
---
+++ conditional statement/GQ4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class GQ4{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a > 0 && b > 0 && c > 0 && a + b + c == 180)
+            System.out.println("Valid Triangle");
+        else
+            System.out.println("Invalid Triangle");
+    }
+}
